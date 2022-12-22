db0c1dbea6265f2a3e09a8e5df4604be35a2a630
Mike Lorzing <48525012+michaellorzing@users.noreply.github.com>
2022-12-22 11:16:41 -0500

    button

--- pages/table.js
+++ pages/table.js
@@ -15,7 +15,12 @@ export default function Table() {
 				<button
 					onClick={(e) => handleClick(e)}
 					className='px-8 py-4 bg-purple-800 text-white font-medium text-lg leading-tight uppercase rounded shadow-md hover:bg-purple-900 hover:shadow-lg focus:bg-purple-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-900 active:shadow-lg transition duration-150 ease-in-out'
-					style={{ position: 'fixed', top: '48%', left: '46%' }}>
+					style={{
+						position: 'fixed',
+						top: '50%',
+						left: '50%',
+						transform: 'translate(-50%,-50%)',
+					}}>
 					SIGN IN WITH DISCORD
 				</button>
 			</div>
